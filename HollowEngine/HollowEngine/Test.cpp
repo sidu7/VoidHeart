23a9b815ca55716fc7310c34460043088d1be6d1
d.rasske <d.rasske@digipen.edu>
2019-09-10 16:23:02 -0700

    Added initial engine and application

---
+++ HollowEngine/HollowEngine/Test.cpp
@@ -0,0 +1,8 @@
+#include "Test.h"
+
+#include <iostream>
+
+void Test::Print()
+{
+	std::cout << "Hello from the other side" << std::endl;
+}
---
+++ HollowEngine/HollowEngine/Test.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace Test
+{
+	__declspec(dllexport) void Print();
+};
