b9dabd8fa804b5e8040ef3fb33a45b6f1e071707
Shantanu <shantanu.chauhan@gmail.com>
2019-09-14 16:04:28 -0700

    Logging Done

--- HollowEngine/HollowEngine/src/Hollow/EntryPoint.h
+++ HollowEngine/HollowEngine/src/Hollow/EntryPoint.h
@@ -6,6 +6,12 @@ extern Hollow::Application* Hollow::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	//This has to be moved out of the main as well later.
+	Hollow::Log::Init();
+	HW_CORE_WARN("The log is ready!");
+	//Example for how to send a variable to log
+	int a = 5;
+	HW_INFO("Oi yoi yoi x {0}", a);
 
 	auto app = Hollow::CreateApplication();
 	app->Run();
---
+++ HollowEngine/HollowEngine/src/Hollow/Log.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Hollow {
+
+	class HOLLOW_API Log
+	{
+	public:
+		static void Init();
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+		
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+//Core log macros
+#define HW_CORE_TRACE(...)  ::Hollow::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define HW_CORE_INFO(...)   ::Hollow::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define HW_CORE_WARN(...)   ::Hollow::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define HW_CORE_ERROR(...)  ::Hollow::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define HW_CORE_FATAL(...)  ::Hollow::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+//Client log macros
+#define HW_TRACE(...)       ::Hollow::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define HW_INFO(...)        ::Hollow::Log::GetClientLogger()->info(__VA_ARGS__)
+#define HW_WARN(...)        ::Hollow::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define HW_ERROR(...)       ::Hollow::Log::GetClientLogger()->error(__VA_ARGS__)
+#define HW_FATAL(...)       ::Hollow::Log::GetClientLogger()->fatal(__VA_ARGS__)
+
