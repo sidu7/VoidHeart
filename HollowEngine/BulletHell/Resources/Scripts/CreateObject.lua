070fc14efb83b2f139f3a7f9e3ff270f1e734b32
drasske <35553333+drasske@users.noreply.github.com>
2019-11-23 18:28:50 -0800

    Messing with player movement script

--- HollowEngine/BulletHell/Resources/Scripts/CreateObject.lua
+++ HollowEngine/BulletHell/Resources/Scripts/CreateObject.lua
@@ -1,4 +1,4 @@
-gameObjectPath = "Resources/Json data/LocalLight.json"
+gameObjectPath = "Resources/Json data/Box2.json"
 
 -- Lua comment
 position = vec3.new(2.0, 1.0, 0.0)
---
+++ HollowEngine/BulletHell/Resources/Scripts/PlayerMovement.lua
@@ -0,0 +1,31 @@
+impulse = vec3.new()
+
+front = fpsCamera.frontDirection
+right = fpsCamera.rightDirection
+
+front.y = 0.0
+right.y = 0.0
+
+if isMoveForward then
+	impulse = impulse + front;
+end
+if isMoveBackward then
+	impulse = impulse - front;	
+end
+if isStrafeLeft then
+	impulse = impulse - right;
+end
+if isStrafeRight then
+	impulse = impulse + right;
+end
+if jump then
+	impulse = impulse + vec3.new(0.0, 20.0, 0.0);
+end
+if dash then
+	dashSpeed = 40.0
+	impulse = impulse + front*dashSpeed
+end
+
+-- Damp Overall Velocity and Rotation
+player.velocity = player.velocity - 0.1 * player.velocity 
+player.angularVelocity = player.angularVelocity - 0.1 * player.angularVelocity 
