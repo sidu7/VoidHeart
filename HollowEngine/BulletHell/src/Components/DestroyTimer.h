0a24194ced46d526891de10d41bc145e4844969d
Shantanu <shantanu.chauhan@gmail.com>
2019-12-09 01:08:31 -0800

    BulletHell and MarbleBall

--- HollowEngine/BulletHell/src/Components/DestroyTimer.h
+++ HollowEngine/BulletHell/src/Components/DestroyTimer.h
@@ -8,10 +8,31 @@ namespace BulletHell
 	{
 		REGISTERCOMPONENT(DestroyTimer);
 	public:
+		/// <summary>
+		/// Initializes the DestroyTimer component.
+		/// </summary>
 		void Init();
+
+		/// <summary>
+		/// Serializes the specified data of the DestroyTimer component from the JSON file.
+		/// </summary>
+		/// <param name="data">JSON object.</param>
 		void Serialize(rapidjson::Value::Object data);
+
+		/// <summary>
+		/// DeSerialize the current DestroyTimer component into a JSON file.
+		/// </summary>
+		/// <param name="writer">The writer of the JSON file.</param>
 		void DeSerialize(rapidjson::Writer<rapidjson::StringBuffer>& writer);
+
+		/// <summary>
+		/// Clears the DestroyTimer component.
+		/// </summary>
 		void Clear();
+
+		/// <summary>
+		/// To set the Debug Display for the DestroyTimer Component.
+		/// </summary>
 		void DebugDisplay();
 
 	public:
--- HollowEngine/BulletHell/src/Components/Movement.h
+++ HollowEngine/BulletHell/src/Components/Movement.h
@@ -7,10 +7,31 @@ namespace BulletHell
 	{
 		REGISTERCOMPONENT(Movement);
 	public:
+		/// <summary>
+		/// Initializes the Movement component.
+		/// </summary>
 		void Init();
+
+		/// <summary>
+		/// Serializes the specified data of the Movement component from the JSON file.
+		/// </summary>
+		/// <param name="data">JSON object.</param>
 		void Serialize(rapidjson::Value::Object data);
+
+		/// <summary>
+		/// DeSerialize the current Movement component into a JSON file.
+		/// </summary>
+		/// <param name="writer">The writer of the JSON file.</param>
 		void DeSerialize(rapidjson::Writer<rapidjson::StringBuffer>& writer);
+
+		/// <summary>
+		/// Clears the Movement component.
+		/// </summary>
 		void Clear();
+
+		/// <summary>
+		/// To set the Debug Display for the Movement Component.
+		/// </summary>
 		void DebugDisplay();
 
 	public:
