e13b9fa6230eb35c5c58247fe7a7e3993988ae76
Aleksey Perfilev <aleksey.p@digipen.edu>
2020-02-08 19:50:34 -0800

    turrets no longer slide if they are pushed, following the player enemy, runnign away from the player enemy

--- HollowEngine/BulletHell/Resources/Scripts/SetupLevel.lua
+++ HollowEngine/BulletHell/Resources/Scripts/SetupLevel.lua
@@ -3,7 +3,10 @@ local coords = firstFloor:GetEntrance():GetCoords()
 
 currentRoom = firstFloor:GetEntranceIndex()
 player = CreatePrefabAtPosition("Player", vec3.new(coords.y * roomSize + roomSize/2, 1.0, coords.x * roomSize + roomSize/2))
-CreatePrefabAtPosition("Enemy", vec3.new(coords.y * roomSize + roomSize/2 + 3, 0.5, coords.x * roomSize + roomSize/2))
+CreatePrefabAtPosition("EnemyFollowLookdir", vec3.new(coords.y * roomSize + roomSize/2 + 3, 0.5, coords.x * roomSize + roomSize/2))
+CreatePrefabAtPosition("EnemyTurretTarget", vec3.new(coords.y * roomSize + roomSize/2 - 3, 0.5, coords.x * roomSize + roomSize/2))
+CreatePrefabAtPosition("EnemyTurretCardinal", vec3.new(coords.y * roomSize + roomSize/2 + 3, 0.5, coords.x * roomSize + roomSize/2 + 3))
+CreatePrefabAtPosition("EnemyRunsawayLookDir", vec3.new(coords.y * roomSize + roomSize/2 - 3, 0.5, coords.x * roomSize + roomSize/2 - 3))
 
 --local coords = firstFloor:GetRoomFromIndex(1):GetCoords()
 local spell = CreatePrefabAtPosition("FireSpell", vec3.new(coords.y * roomSize + roomSize/2 +5, 1.0, coords.x * roomSize + roomSize/2+5))
