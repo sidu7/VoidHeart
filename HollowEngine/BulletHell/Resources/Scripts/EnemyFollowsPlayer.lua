4908d2060f335557b402f40adaacfb23ab41d686
Aleksey Perfilev <aleksey.p@digipen.edu>
2020-02-11 16:57:59 -0800

    player properly rotates now, diagonal shooting script, enemies now collide with each other and when they collide with the player player and enemy pushed away

--- HollowEngine/BulletHell/Resources/Scripts/EnemyFollowsPlayer.lua
+++ HollowEngine/BulletHell/Resources/Scripts/EnemyFollowsPlayer.lua
@@ -35,11 +35,21 @@ function FollowPlayer()
 	    rot = vec3.new(0.0, degree + 180, 0.0)
         transform:Rotate(rot)
     end
-    
-    -- setting the velocity
-	local body = gameObject:GetBody()
+
+    local body = gameObject:GetBody()
     body.velocity = enemySpeed * vec3.new(xDirNorm, 0.0, zDirNorm)
     
+    -- collision handling
+    if (dirLength < 1.0) then
+        local impulseStrength = vec3.new(1000, 0, 1000)
+        local impulse = vec3.new(xDirNorm, 0.0, zDirNorm)
+        impulse  = impulse * impulseStrength
+        ApplyLinearImpulse(player, impulse)
+
+        local impulseReverse = vec3.new(-xDirNorm, 0.0, -zDirNorm)
+        impulseReverse = impulseReverse * impulseStrength
+        ApplyLinearImpulse(gameObject, impulseReverse)
+    end
 end
 
 function Update()
