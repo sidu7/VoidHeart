7921cd4093dd7cbed43b345f9fbe8c168812f6fc
d.rasske <d.rasske@digipen.edu>
2020-03-05 13:37:40 -0800

    Over dramatic shadows

--- HollowEngine/BulletHell/Resources/Scripts/LightFollow.lua
+++ HollowEngine/BulletHell/Resources/Scripts/LightFollow.lua
@@ -1,15 +1,27 @@
 function SetLightPosition()
 	-----------------------------------------
     -- playtesting vars
-	local positionOffset = vec3.new(3.0, 5.0, 2.0)
+	local positionOffset = vec3.new(10.0, 10.0, 25.0)
 	local lookAtOffset = vec3.new(0.0, 0.0, 0.0)
+	local t = 0.05
 	-----------------------------------------	
 	local transform = gameObject:GetTransform()
 	
-	-- Light position is 
-	transform.position = camera:GetTransform().position + positionOffset
+	--player = CreatePrefabAtPosition("Player", vec3.new(coords.y * roomSize + roomSize/2, 0.5, coords.x * roomSize + roomSize/2))
+	local floor = GetDungeonFloor(currentFloor)
+	local currentRoomObj = floor:GetRoomFromIndex(currentRoom)
+	local coords = currentRoomObj:GetCoords()
+	-- Light position is based on current room
+	position = vec3.new(coords.y * roomSize + roomSize/2, 0.0, coords.x * roomSize + roomSize/2) + positionOffset
+	transform.position = transform.position + t * (position - transform.position)
+	
+	-- Set light to look at center of room
+	
 	local light = gameObject:GetLight()
-	light.lookAt = player:GetTransform().position + lookAtOffset
+	local lookAtPos = vec3.new(coords.y * roomSize + roomSize/2, 0.0, coords.x * roomSize + roomSize/2) + lookAtOffset
+	--local lookAt = vec3.new(0.0, 0.0, 0.0)
+	--lookAt = lookAtPos + lookAtOffset
+	light.lookAt = light.lookAt + t * (lookAtPos - light.lookAt) 
 end
 
 function UpdateLight()
