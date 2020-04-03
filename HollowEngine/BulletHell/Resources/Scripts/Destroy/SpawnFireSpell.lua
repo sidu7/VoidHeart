1ac2d0d14c92ace0f017007004b2e4e7e6ba2921
drasske <35553333+drasske@users.noreply.github.com>
2020-04-02 22:18:28 -0700

    Using player height to spawn collectable spells

--- HollowEngine/BulletHell/Resources/Scripts/Destroy/SpawnFireSpell.lua
+++ HollowEngine/BulletHell/Resources/Scripts/Destroy/SpawnFireSpell.lua
@@ -1,6 +1,6 @@
 function SpawnFireSpell ()
 	local go = CreatePrefabAtPosition("FireSpell", gameObject:GetTransform().position)
-	go:GetBody().position.y = 1.5
+	go:GetBody().position.y = player:GetBody().position.y
 	go:GetTransform().position.y = go:GetBody().position.y
 end
 
--- HollowEngine/BulletHell/Resources/Scripts/Destroy/SpawnWaterSpell.lua
+++ HollowEngine/BulletHell/Resources/Scripts/Destroy/SpawnWaterSpell.lua
@@ -1,6 +1,6 @@
 function SpawnWaterSpell ()
 	local go = CreatePrefabAtPosition("WaterSpell", gameObject:GetTransform().position)
-	go:GetBody().position.y = 1.5
+	go:GetBody().position.y = player:GetBody().position.y
 	go:GetTransform().position.y = go:GetBody().position.y
 end
 
